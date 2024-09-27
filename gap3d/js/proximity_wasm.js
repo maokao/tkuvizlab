aff3475b30e5fc3c78c39d15fa1047b086aadc78
Chiun-How Kao <maokao25@gmail.com>
2024-09-27 16:39:45 +0800

    update

--- gap3d/js/proximity_wasm.js
+++ gap3d/js/proximity_wasm.js
@@ -5,37 +5,6 @@ var Module = typeof Module !== 'undefined' ? Module : {};
 
 var runProximityWASM = Module.cwrap("computeProximity", null, ["number", "number", "number", "number", "number", "number", "number"]); // void function
 
-// 生成之 array 輸出成 txt
-function exportArrayToTxt(array, filename = 'data.txt') {
-    // 檢查是否找到對象
-    if (!array || array.length === 0) {
-        console.log("Element " + filename + " not found.");
-        return;
-    }
-
-    // 檢測 array 維度
-    const isTwoDimensional = Array.isArray(array[0]);
-    let text;
-    if (isTwoDimensional) {
-        // 如果是二維 array 每個 array 元素佔一行 行內元素以 tab 鍵相隔分隔
-        text = array.map(row => row.join('\t')).join('\n');
-    } else {
-        // 如果是一維 array 直接用 tab 鍵連接所有元素
-        text = array.join('\t');
-    }
-
-    // 建立一個 blob 對象 指定內容類型為純文字
-    const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
-
-    // 建立一個隱藏的 a 標籤 用於觸發下載
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = filename; // 設定下載檔案的檔案名
-    document.body.appendChild(a); // 新增 a 標籤到 body
-    a.click(); // 觸發下載
-    document.body.removeChild(a); // 下載後移除 a 標籤
-}
-
 function runProximity(proxType, side, isContainMissingValue, axis) {
   
         if (axis == 0) {
